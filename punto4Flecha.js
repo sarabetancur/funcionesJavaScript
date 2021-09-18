56fb81c759d865767aadcdc56adbc23a33cf8c11
Sara Betancur <sarabetancurocampo22@gmail.com>
2021-09-18 12:46:10 -0500

    Se actualiza punto 4

--- punto4Flecha.js
+++ punto4Flecha.js
@@ -7,12 +7,11 @@ let calcularEnergiaNegativa=(sable,energiaNegativa)=>{
        for (let i = 0; i < sable.length; i++) {
               if(sable[i]<0){
                energiaNegativa++
-              }  
+              }          
        }
        return(energiaNegativa)
        
    }
      
 console.log(`La cantidad de energía negativa es: ${calcularEnergiaNegativa(sable,energiaNegativa)} Sables`);
-
 
